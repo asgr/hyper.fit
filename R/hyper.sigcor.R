2f622f14cc08c061a60353695a0fd5c8a721c2bf
Aaron Robotham <aaron.robotham@uwa.edu.au>
2014-09-19 21:49:16 +0200

    Document fixes.

--- R/hyper.sigcor.R
+++ R/hyper.sigcor.R
@@ -1,4 +1,4 @@
-sigcor=function(N,parmDF){
+hyper.sigcor=function(N,parmDF){
   bias2unbias=(sqrt((N-parmDF)/2)*gamma((N-parmDF)/2))/gamma((N-parmDF+1)/2)
   samp2pop=sqrt(N/(N-parmDF))
   return=c(bias2unbias=bias2unbias,samp2pop=samp2pop,sampbias2popunbias=bias2unbias*samp2pop)
