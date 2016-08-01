b55317784e8f0d9228824812a5b725c0fd52e4f2
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-08-01 15:11:45 +0800

    Minor changes to get on CRAN, now that LaplacesDemon is a CRAN package.

--- R/hyper.convert.R
+++ R/hyper.convert.R
@@ -117,9 +117,9 @@ hyper.convert=function(parm,coord,beta=0,scat=0,in.coord.type='alpha',out.coord.
   return=out
 }
 
-convert=function(x,...){UseMethod("convert")}
+convert=function(x,coord.type='alpha',scat.type='vert.axis',vert.axis,...){UseMethod("convert")}
 
-convert.hyper.plane.param=function(x,coord.type='alpha',scat.type='vert.axis',vert.axis){
+convert.hyper.plane.param=function(x,coord.type='alpha',scat.type='vert.axis',vert.axis,...){
   parm=x$parm
   in.coord.type=x$coord.type
   in.scat.type=x$scat.type
